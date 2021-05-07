e3e1ae82a3d6ea5abae887a43498cea9d5bf58ca
Ruben Poppe <ruben.poppe@hotmail.com>
2021-05-07 10:45:48 +0200

    add layers

--- js/map.js
+++ js/map.js
@@ -1,20 +1,40 @@
-const map = L.map('map').setView([51.1018, 3.6351], 10);
+(async () => {
+	const tiles = L.tileLayer(
+		'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+		{
+			maxZoom: 18,
+			attribution:
+				'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+				'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			id: 'mapbox/streets-v11',
+			tileSize: 512,
+			zoomOffset: -1,
+		}
+	);
 
-L.tileLayer(
-	'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
-	{
-		maxZoom: 18,
-		attribution:
-			'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1,
-	}
-).addTo(map);
+	const map = L.map('map', {
+		center: [51.1018, 3.6351],
+		zoom: 10,
+		layers: [tiles],
+	});
 
-let filename = 'toer_recr_netwerk';
+	const filenames = [
+		'BFF',
+		'LFF',
+		'fietspotentieel_volledig',
+		'Fietssnelweg',
+		'toer_recr_netwerk',
+	];
 
-fetch(`/data/${filename}.geojson`)
-	.then((res) => res.json())
-	.then((json) => L.geoJSON(json).addTo(map));
+	const controlLayers = L.control
+		.layers({}, {}, { collapsed: false })
+		.addTo(map);
+		
+	const getData = async () => {
+		filenames.forEach(async (filename) => {
+			const res = await fetch(`/data/${filename}.geojson`);
+			controlLayers.addOverlay(L.geoJSON(await res.json()), filename);
+		});
+	};
+	await getData();
+})();
