240fd29398424b149cf081fa306b908c1914c8f8
Ruben Poppe <ruben.poppe@hotmail.com>
2021-05-07 11:00:33 +0200

    add light/dark background

--- js/map.js
+++ js/map.js
@@ -1,7 +1,5 @@
 (async () => {
-	const tiles = L.tileLayer(
-		'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
-		{
+	const tilesOptions = {
 			maxZoom: 18,
 			attribution:
 				'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -9,13 +7,22 @@
 			id: 'mapbox/streets-v11',
 			tileSize: 512,
 			zoomOffset: -1,
-		}
+	};
+
+	const light = L.tileLayer(
+		'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+		{ ...tilesOptions, id: 'mapbox/light-v10' }
+	);
+
+	const dark = L.tileLayer(
+		'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+		{ ...tilesOptions, id: 'mapbox/dark-v10' }
 	);
 
 	const map = L.map('map', {
 		center: [51.1018, 3.6351],
 		zoom: 10,
-		layers: [tiles],
+		layers: [light],
 	});
 
 	const filenames = [
@@ -27,7 +34,11 @@
 	];
 
 	const controlLayers = L.control
-		.layers({}, {}, { collapsed: false })
+		.layers(
+			{ 'Lichte achtergrond': light, 'Donkere achtergrond': dark },
+			{},
+			{ collapsed: false }
+		)
 		.addTo(map);
 		
 	const getData = async () => {
